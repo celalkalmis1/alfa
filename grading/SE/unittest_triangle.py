a45ac02418509a90d71f7a0790d87afff46d020a
celalkalmis1 <celalkalmisfb@gmail.com>
2026-06-17 15:29:30 +0200

    Some changes

--- grading/SE/unittest_triangle.py
+++ grading/SE/unittest_triangle.py
@@ -1,16 +1,37 @@
+import unittest
+
 def check_triangle(a, b, c):
-    # Check if all sides are greater than 0
+   
     if a <= 0 or b <= 0 or c <= 0:
         return False
         
-    # Check the triangle rule: sum of two sides must be greater than the third
+   
     if (a + b > c) and (a + c > b) and (b + c > a):
         return True
     else:
         return False
 
-# Testing the function
-print(check_triangle(3, 4, 5))   # This will print True
-print(check_triangle(1, 2, 3))   # This will print False
-print(check_triangle(10, 2, 2))  # This will print False
-print(check_triangle(0, 5, 5))   # This will print False
+class TestCheckTriangle(unittest.TestCase):
+
+    def test_valid_triangle(self):
+      
+        self.assertTrue(check_triangle(3, 4, 5))
+
+    def test_degenerate_triangle(self):
+        
+        self.assertFalse(check_triangle(1, 2, 3))
+
+    def test_invalid_triangle(self):
+        
+        self.assertFalse(check_triangle(10, 2, 2))
+
+    def test_zero_side_triangle(self):
+        
+        self.assertFalse(check_triangle(0, 5, 5))
+
+    def test_negative_side_triangle(self):
+       
+        self.assertFalse(check_triangle(-3, 4, 5))
+
+if __name__ == '__main__':
+    unittest.main()
